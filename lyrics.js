8c0f4b68aefa287b23f1d3e78f33b82eca747753
singhremo00-commits <singhremo00@gmail.com>
2026-09-24 18:22:15 +0530

    Create lyrics.js with song lyrics database
    Added a database of song lyrics for Remo Singh's 34 songs.

---
+++ lyrics.js
@@ -0,0 +1,139 @@
+// Remo Singh - All 34 Songs Official Lyrics Database
+const songLyricsMap = {
+  "Raati": `
+(Raati gaane ke bol yahan paste karein)
+  `,
+
+  "Raati 2": `
+(Raati 2 ke bol yahan paste karein)
+  `,
+
+  "Nigshing Owri": `
+(Nigshing Owri ke bol yahan paste karein)
+  `,
+
+  "T Sara Nai": `
+(T Sara Nai ke bol yahan paste karein)
+  `,
+
+  "T Sara Nai 2": `
+(T Sara Nai 2 ke bol yahan paste karein)
+  `,
+
+  "Aji Tore Peya": `
+(Aji Tore Peya ke bol yahan paste karein)
+  `,
+
+  "Ahesile": `
+(Ahesile ke bol yahan paste karein)
+  `,
+
+  "Khalkoruri": `
+(Khalkoruri ke bol yahan paste karein)
+  `,
+
+  "Monor Dairy": `
+(Monor Dairy ke bol yahan paste karein)
+  `,
+
+  "Monor Yaari": `
+(Monor Yaari ke bol yahan paste karein)
+  `,
+
+  "Tor Sale": `
+(Tor Sale ke bol yahan paste karein)
+  `,
+
+  "Nais Durey Durey": `
+(Nais Durey Durey ke bol yahan paste karein)
+  `,
+
+  "Hopon": `
+(Hopon ke bol yahan paste karein)
+  `,
+
+  "Ti": `
+(Ti ke bol yahan paste karein)
+  `,
+
+  "Ti 2": `
+(Ti 2 ke bol yahan paste karein)
+  `,
+
+  "Banar Moyango": `
+(Banar Moyango ke bol yahan paste karein)
+  `,
+
+  "Thawri Impani Oya": `
+(Thawri Impani Oya ke bol yahan paste karein)
+  `,
+
+  "Nigisga Durey": `
+(Nigisga Durey ke bol yahan paste karein)
+  `,
+
+  "Nai Kuno Bana": `
+(Nai Kuno Bana ke bol yahan paste karein)
+  `,
+
+  "Nai Kuno Bana 2": `
+(Nai Kuno Bana 2 ke bol yahan paste karein)
+  `,
+
+  "Thaitai Hudda Mor Ti": `
+(Thaitai Hudda Mor Ti ke bol yahan paste karein)
+  `,
+
+  "Morgoi Nagoi Ti": `
+(Morgoi Nagoi Ti ke bol yahan paste karein)
+  `,
+
+  "Thaile Ti Kumpey": `
+(Thaile Ti Kumpey ke bol yahan paste karein)
+  `,
+
+  "Asha Nai": `
+(Asha Nai ke bol yahan paste karein)
+  `,
+
+  "Jauriga Nai Beliya": `
+(Jauriga Nai Beliya ke bol yahan paste karein)
+  `,
+
+  "Aaji Dehiya Tore": `
+(Aaji Dehiya Tore ke bol yahan paste karein)
+  `,
+
+  "T Naile": `
+(T Naile ke bol yahan paste karein)
+  `,
+
+  "Banar Yaari": `
+(Banar Yaari ke bol yahan paste karein)
+  `,
+
+  "Maya Ti": `
+(Maya Ti ke bol yahan paste karein)
+  `,
+
+  "Gussa Na Ho": `
+(Gussa Na Ho ke bol yahan paste karein)
+  `,
+
+  "Monor D": `
+(Monor D ke bol yahan paste karein)
+  `,
+
+  "Monor Yari": `
+(Monor Yari ke bol yahan paste karein)
+  `,
+
+  "Mattou Me Kisade": `
+(Mattou Me Kisade ke bol yahan paste karein)
+  `,
+
+  "J Tor Shriti": `
+(J Tor Shriti ke bol yahan paste karein)
+  `
+};
+
